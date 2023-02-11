ac8162f050cf627d64f998e5c6d89cfee5e797e9
mikanogami <mikanogami@gmail.com>
2023-02-10 20:25:21 -0800

    Websocket works with frontend code: websocket-example

--- websocket.py
+++ websocket.py
@@ -6,6 +6,93 @@ import mediapipe as mp
 import time
 import functools
 
+"""
+async def handler(websocket, path):
+    while True:
+        await websocket.send("test")
+
+async def main():
+    async with websockets.serve(handler, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+"""
+
+
+async def handler(websocket, path):
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands(static_image_mode=False,
+                        max_num_hands=2,
+                        min_detection_confidence=0.5,
+                        min_tracking_confidence=0.5)
+    mpDraw = mp.solutions.drawing_utils
+    cap = cv2.VideoCapture(0) # use VideoCapture(0) to use local (built in) webcam
+    # cap = cv2.VideoCapture(1) # use VideoCapture(1) to use a USB webcam
+    while True and websocket.open:
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+        message = ""
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                for id, lm in enumerate(handLms.landmark):
+                    #print(id,lm)
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x *100), int(lm.y*100)
+                    if id == 8:
+                        # to get the purple line that tracks the index finger, uncomment the following two lines
+                        # cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+                        # cv2.line(img, (0,0), (cx,cy), (255,0,255), 3)
+                        # print(cx, cy)
+                        message = str(cx)+' '+str(cy)
+                        break
+
+        await websocket.send(message)
+
+
+async def handler1(websocket, path):
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands(static_image_mode=False,
+                        max_num_hands=2,
+                        min_detection_confidence=0.5,
+                        min_tracking_confidence=0.5)
+    mpDraw = mp.solutions.drawing_utils
+    cap = cv2.VideoCapture(0) # use VideoCapture(0) to use local (built in) webcam
+    # cap = cv2.VideoCapture(1) # use VideoCapture(1) to use a USB webcam
+    while True and websocket.open:
+        await websocket.send("testing")
+        pTime = 0
+        cTime = 0
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+        #print(results.multi_hand_landmarks)
+
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                for id, lm in enumerate(handLms.landmark):
+                    #print(id,lm)
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x *100), int(lm.y*100)
+                    if id == 8:
+                        # to get the purple line that tracks the index finger, uncomment the following two lines
+                        # cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+                        # cv2.line(img, (0,0), (cx,cy), (255,0,255), 3)
+                        # print(cx, cy)
+                        message = str(cx)+' '+str(cy)
+                        await websocket.send(message)
+                        # print(message)
+                        # loop.call_soon_threadsafe(queue.put_nowait, message)
+
+        cv2.waitKey(1)
+        
+    # print("exit")
+
+"""
 async def echo(queue, websocket):
     # print("enter")
     while True and websocket.open:
@@ -52,9 +139,9 @@ def blocking(loop, queue):
                         # print(cx, cy)
                         message = str(cx)+' '+str(cy)
                         # print(message)
-                        loop.call_soon_threadsafe(queue.put_nowait, message)
+                        # loop.call_soon_threadsafe(queue.put_nowait, message)
 
-                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+                # mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
         # cTime = time.time()
         # fps = 1/(cTime-pTime)
@@ -62,19 +149,14 @@ def blocking(loop, queue):
 
         # cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 
-        cv2.imshow("Image", img)
+        # cv2.imshow("Image", img)
         cv2.waitKey(1)
+"""
+
 
 async def main():
-    queue = asyncio.Queue()
-    fake_queue = asyncio.Queue()
-    loop = asyncio.get_running_loop()
-
-    blocking_fut = loop.run_in_executor(None, blocking, loop, queue)
-    nonblocking_task = loop.create_task(start_socket(queue))
-
-    running = True  # use whatever exit condition
-    while running:
-        await fake_queue.get()
+    async with websockets.serve(handler, "localhost", 8765):
+        await asyncio.Future()  # run forever
        
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
