466ec34ca278692c59c66bcf41c15f69043075ae
Beatrice Tam <72827596+btam6293@users.noreply.github.com>
2023-03-28 16:48:34 -0700

    cursor adjustement

--- serial_input.py
+++ serial_input.py
@@ -14,12 +14,13 @@ if not ser.is_open:
 
 async def handler(websocket, path):
     print("open socket")
+    sensor_data = ""
 
     while True and websocket.open and ser.open:
         msg = ser.readline().decode()
         if (msg != ""):
             sensor_data = msg
-            # print(sensor_data)
+            print(sensor_data)
         
         await websocket.send(sensor_data)
 
